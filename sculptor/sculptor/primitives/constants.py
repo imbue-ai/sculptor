2842c04e47a9e18e3fb6757ebf79835b5ca221ec
Saeed Noursalehi <saeed.noursalehi@imbue.com>
2026-06-15 13:25:33 -0700

    Bring sculptor primitives into compliance with backend review rules

--- sculptor/sculptor/primitives/constants.py
+++ sculptor/sculptor/primitives/constants.py
@@ -4,5 +4,5 @@ from sculptor.primitives.ids import OrganizationReference
 from sculptor.primitives.ids import UserReference
 
 MESSAGE_LOG_TYPE: Final = "message_log"
-ANONYMOUS_USER_REFERENCE = UserReference("777777777")
-ANONYMOUS_ORGANIZATION_REFERENCE = OrganizationReference("77777777-7777-7777-7777-777777777777")
+ANONYMOUS_USER_REFERENCE: Final = UserReference("777777777")
+ANONYMOUS_ORGANIZATION_REFERENCE: Final = OrganizationReference("77777777-7777-7777-7777-777777777777")
